4b369a3c89275b8d29cfebee3eb108a937480ddd
jimmiebergmann <jimmiebergmann@gmail.com>
2013-05-04 00:33:28 +0200

    Cleaned up the Linux build.

--- include/Bit/DataTypes.hpp
+++ include/Bit/DataTypes.hpp
@@ -1,23 +1,23 @@
 // ///////////////////////////////////////////////////////////////////////////
 // Copyright (C) 2013 Jimmie Bergmann - jimmiebergmann@gmail.com
-// 
+//
 // This software is provided 'as-is', without any express or
 // implied warranty. In no event will the authors be held
 // liable for any damages arising from the use of this software.
-// 
+//
 // Permission is granted to anyone to use this software for any purpose,
 // including commercial applications, and to alter it and redistribute
 // it freely, subject to the following restrictions:
-// 
+//
 // 1. The origin of this software must not be misrepresented;
 //    you must not claim that you wrote the original software.
 //    If you use this software in a product, an acknowledgment
 //    in the product documentation would be appreciated but
 //    is not required.
-// 
+//
 // 2. Altered source versions must be plainly marked as such,
 //    and must not be misrepresented as being the original software.
-// 
+//
 // 3. This notice may not be removed or altered from any
 //    source distribution.
 // ///////////////////////////////////////////////////////////////////////////
@@ -58,8 +58,8 @@ typedef unsigned short		BIT_UINT16;
 typedef signed short		BIT_SINT16;
 typedef unsigned int		BIT_UINT32;
 typedef signed int			BIT_SINT32;
-typedef unsigned __int64	BIT_UINT64;
-typedef signed __int64		BIT_SINT64;
+typedef unsigned long long	BIT_UINT64;
+typedef signed long long	BIT_SINT64;
 typedef float				BIT_FLOAT32;
 typedef double				BIT_FLOAT64;
 typedef BIT_UCHAR8			BIT_BYTE;
