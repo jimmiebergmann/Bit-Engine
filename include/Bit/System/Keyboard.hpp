aa9850f10a4358641ed945616b3df5bd00b9fe4d
jimmiebergmann <jimmiebergmann@gmail.com>
2013-07-27 01:49:44 +0200

    Git a somehow working keyboard, (not the window one)

--- include/Bit/System/Keyboard.hpp
+++ include/Bit/System/Keyboard.hpp
@@ -39,9 +39,98 @@ namespace Bit
         enum eKey
         {
             Key_None = 0,
+
+            // Alphabet keys
             Key_A,
             Key_B,
             Key_C,
+            Key_D,
+            Key_E,
+            Key_F,
+            Key_G,
+            Key_H,
+            Key_I,
+            Key_J,
+            Key_K,
+            Key_L,
+            Key_M,
+            Key_N,
+            Key_O,
+            Key_P,
+            Key_Q,
+            Key_R,
+            Key_S,
+            Key_T,
+            Key_U,
+            Key_V,
+            Key_W,
+            Key_X,
+            Key_Y,
+            Key_Z,
+
+            // Numeric keys
+            Key_0,
+            Key_1,
+            Key_2,
+            Key_3,
+            Key_4,
+            Key_5,
+            Key_6,
+            Key_7,
+            Key_8,
+            Key_9,
+
+            // Function Keys
+            Key_F1,
+            Key_F2,
+            Key_F3,
+            Key_F4,
+            Key_F5,
+            Key_F6,
+            Key_F7,
+            Key_F8,
+            Key_F9,
+            Key_F10,
+            Key_F11,
+            Key_F12,
+
+            // Arrow keys
+            Key_Down,
+            Key_Left,
+            Key_Right,
+            Key_Up,
+
+            // System keys
+            Key_Alt,
+            Key_Backspace,
+            Key_Break,
+            Key_Capslock,
+            Key_Control,
+            Key_Delete,
+            Key_End,
+            key_Espace,
+            Key_Home,
+            Key_Insert,
+            Key_PageDown,
+            Key_PageUp,
+            Key_Print,
+            Key_Return,
+            Key_ScrollLock,
+            Key_Shift,
+            Key_Space,
+            key_Super,
+            Key_Tab,
+
+            // Media keys
+            Key_Mute,
+            Key_Next,
+            Key_Play,
+            Key_Previous,
+            Key_Stop,
+            Key_VolumeHigh,
+            Key_VolumeLow,
+
+            // Key counter (not a key)
             Key_Count
         };
 
